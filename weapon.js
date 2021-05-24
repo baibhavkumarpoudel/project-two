dc3537f81145fd43141a629abde8adb682e67bdd
Baibhav K Poudel <bkpoudel@up.com>
2021-05-24 10:00:36 -0500

    working on weapon.js file along with server.js

---
+++ weapon.js
@@ -0,0 +1,39 @@
+const weapon = [
+    {
+        name: "Ak-47",
+        description: "Very reliable automatic assault rifle chambered in 7.62mm Soviet. Large caliber ammunition requires skill to control recoil.",
+        img: "https://www.krale.shop/media/catalog/product/c/y/cybergun-kalashnikov-ak47-_1_.jpg"
+    },
+    {
+        name: "FN Scar 17",
+        description: "Large caliber, fully automatic assualt rifle that provides high damage over long ranges.",
+        img: "https://fnamerica.com/wp-content/uploads/2012/09/FN_SCAR_17S_Rotators_1-1200x598.png"
+    },
+    {
+        name: "Ak-47",
+        description: "Very reliable automatic assault rifle chambered in 7.62mm Soviet. Large caliber ammunition requires skill to control recoil.",
+        img: "https://www.krale.shop/media/catalog/product/c/y/cybergun-kalashnikov-ak47-_1_.jpg"
+    },
+    {
+        name: "Ak-47",
+        description: "Very reliable automatic assault rifle chambered in 7.62mm Soviet. Large caliber ammunition requires skill to control recoil.",
+        img: "https://www.krale.shop/media/catalog/product/c/y/cybergun-kalashnikov-ak47-_1_.jpg"
+    },
+    {
+        name: "Ak-47",
+        description: "Very reliable automatic assault rifle chambered in 7.62mm Soviet. Large caliber ammunition requires skill to control recoil.",
+        img: "https://www.krale.shop/media/catalog/product/c/y/cybergun-kalashnikov-ak47-_1_.jpg"
+    },
+    {
+        name: "Ak-47",
+        description: "Very reliable automatic assault rifle chambered in 7.62mm Soviet. Large caliber ammunition requires skill to control recoil.",
+        img: "https://www.krale.shop/media/catalog/product/c/y/cybergun-kalashnikov-ak47-_1_.jpg"
+    },
+    {
+        name: "Ak-47",
+        description: "Very reliable automatic assault rifle chambered in 7.62mm Soviet. Large caliber ammunition requires skill to control recoil.",
+        img: "https://www.krale.shop/media/catalog/product/c/y/cybergun-kalashnikov-ak47-_1_.jpg"
+    }
+];
+
+module.exports = weapon;
