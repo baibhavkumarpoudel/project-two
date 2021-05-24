b700a9c349851a34bfdce3f1b27ca7ac553f0fb4
Baibhav K Poudel <bkpoudel@up.com>
2021-05-24 11:24:41 -0500

    migrated table and it is visible on psql!

--- migrations/20210524160846-create-warrior.js
+++ migrations/20210524160846-create-warrior.js
@@ -12,18 +12,21 @@ module.exports = {
         type: Sequelize.STRING
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       password: {
         type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: new Date()
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: new Date()
       }
     });
   },
